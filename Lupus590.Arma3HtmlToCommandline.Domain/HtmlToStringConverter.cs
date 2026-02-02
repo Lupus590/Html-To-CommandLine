6df9ff85e28bb3ac0b486bcc70c8e7017590786c
Lupus590 <9790321+Lupus590@users.noreply.github.com>
2026-02-02 16:54:28 +0000

    bones of a test

---
+++ Lupus590.Arma3HtmlToCommandline.Domain/HtmlToStringConverter.cs
@@ -0,0 +1,14 @@
+﻿namespace Lupus590.Arma3HtmlToCommandline.Domain;
+
+public interface IHtmlToStringConverter
+{
+    public string ProcessFile(string filePath);
+}
+
+public class HtmlToStringConverter : IHtmlToStringConverter
+{
+	public string ProcessFile(string filePath)
+	{
+		throw new NotImplementedException();
+	}
+}
