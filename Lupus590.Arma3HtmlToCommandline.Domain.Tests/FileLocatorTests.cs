38778987a061ede729a6f2ee0007c06a86302031
Lupus590 <9790321+Lupus590@users.noreply.github.com>
2026-08-30 12:12:42 +0100

    file locator tests

---
+++ Lupus590.Arma3HtmlToCommandline.Domain.Tests/FileLocatorTests.cs
@@ -0,0 +1,30 @@
+﻿using HtmlAgilityPack;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace Lupus590.Arma3HtmlToCommandline.Domain.Tests;
+
+public class FileLocatorTests
+{
+    [Fact]
+    public void WhenFindArmaModlistHtmlFiles_ReturnsEnumerableOfFiles()
+    {
+        var testFilePath = "../../../testModlist.html";
+        
+        var serviceProvider = GetServiceProvider();
+        
+        var sut = serviceProvider.GetRequiredService<IFileLocator>();
+        var result = sut.FindArmaModlistHtmlFiles().ToList();
+
+        result.Count().ShouldBe(1);
+        result.First().ShouldBe("testModlist.html");
+    }
+
+    private static ServiceProvider GetServiceProvider()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddTransient<IFileLocator, FileLocator>()
+            .AddLogging();
+        return serviceCollection.BuildServiceProvider();
+    }
+}
